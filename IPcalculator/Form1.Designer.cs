55f7201c8a8269135e3948ceb3e0a8c88baefb2a
sherkunkov@gmail.com <sherkunkov@gmail.com>
2024-03-05 22:52:16 +0800

    inprogress

--- IPcalculator/Form1.Designer.cs
+++ IPcalculator/Form1.Designer.cs
@@ -30,13 +30,15 @@
         {
             this.rtbIPinput = new System.Windows.Forms.RichTextBox();
             this.labelIpInfo = new System.Windows.Forms.Label();
+            this.cbMask = new System.Windows.Forms.ComboBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // rtbIPinput
             // 
             this.rtbIPinput.Location = new System.Drawing.Point(12, 12);
             this.rtbIPinput.Name = "rtbIPinput";
-            this.rtbIPinput.Size = new System.Drawing.Size(411, 38);
+            this.rtbIPinput.Size = new System.Drawing.Size(314, 39);
             this.rtbIPinput.TabIndex = 0;
             this.rtbIPinput.Text = "";
             // 
@@ -44,17 +46,39 @@
             // 
             this.labelIpInfo.AutoSize = true;
             this.labelIpInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelIpInfo.Location = new System.Drawing.Point(12, 53);
+            this.labelIpInfo.Location = new System.Drawing.Point(12, 54);
             this.labelIpInfo.Name = "labelIpInfo";
             this.labelIpInfo.Size = new System.Drawing.Size(92, 32);
             this.labelIpInfo.TabIndex = 1;
             this.labelIpInfo.Text = "label1";
+            // 
+            // cbMask
+            // 
+            this.cbMask.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cbMask.FormattingEnabled = true;
+            this.cbMask.IntegralHeight = false;
+            this.cbMask.Location = new System.Drawing.Point(332, 12);
+            this.cbMask.Name = "cbMask";
+            this.cbMask.Size = new System.Drawing.Size(91, 39);
+            this.cbMask.TabIndex = 2;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(348, 415);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(435, 450);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.cbMask);
             this.Controls.Add(this.labelIpInfo);
             this.Controls.Add(this.rtbIPinput);
             this.Name = "Form1";
@@ -68,6 +92,8 @@
 
         private System.Windows.Forms.RichTextBox rtbIPinput;
         private System.Windows.Forms.Label labelIpInfo;
+        private System.Windows.Forms.ComboBox cbMask;
+        private System.Windows.Forms.Button btnCalculate;
     }
 }
 
--- IPcalculator/Form1.cs
+++ IPcalculator/Form1.cs
@@ -12,11 +12,62 @@ namespace IPcalculator
 {
     public partial class Form1 : Form
     {
+        int[] ip;
+        int mask;
+        string ipString = "192.168.100.0";
         public Form1()
         {
             InitializeComponent();
+            FillcbMask();
+            ip = new int[4];
         }
 
+        void FillcbMask()
+        {
+            for (int i = 0; i < 32; i++)
+            {
+                cbMask.Items.Add(i + 1);
+            }
+            cbMask.SelectedItem = 24;
+        }
 
+        void ConvertMaskToBinaryForm()
+        {
+            string maskString = "";
+            for(int i = 0;i < 32;i++)
+            {
+                maskString += i < Convert.ToInt32(cbMask.SelectedItem.ToString()) ? 1 : 0;
+            }
+        }
+
+        void ConvertIpToBinaryForm()
+        {
+            string ipStringInput = ipString;
+            for (int i = 0; i < ipStringInput.Length; i++)
+            {
+                //maskString += i < Convert.ToInt32(cbMask.SelectedItem.ToString()) ? 1 : 0;
+            }
+        }
+
+        void ConvertIpToDecimalForm()
+        {
+            string ipStringInput = ipString; 
+            for(int i = 0; i < 4; i++)
+            {
+                ip[i] = Convert.ToInt32(ipStringInput.Split('.')[i]);
+            }
+        }
+
+        void Calculate()
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            ConvertIpToDecimalForm();
+            string output = ip[0].ToString() + ip[1].ToString() + ip[2].ToString() + ip[3].ToString(); ;
+            labelIpInfo.Text = output;
+        }
     }
 }
